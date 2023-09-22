17fbccc4a483d0f56f8d051e5fb1843e817a4a17
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2023-09-22 18:56:02 +0530

    feat: working matrix effect
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Matrix Effect
+
+See it running at https://haribala.dev/the-matrix-effect
+
+The iconic effect seen in the movie The Matrix (1999).
+
+<img src="./sunglasses.webp"/>
---
+++ docs/main.js
@@ -0,0 +1,78 @@
+const get_japanese = () => {
+    const r = [];
+    for (let x = 0x4e00; x < 0x4e20; x++) r.push(x);
+    return r;
+};
+const char_set = (
+    "abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_+=" +
+    String.fromCodePoint(...get_japanese())
+).split("");
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const random_bool = (p) => Math.random() < p;
+const random_int = (n) => Math.floor(Math.random() * n) % n;
+const choose_random = (xs) => xs[random_int(xs.length)];
+const reset = (div_matrix, num_rows, num_cols) => {
+    const grid = [];
+    for (let r = 0; r < num_rows; r++) {
+        const row = document.createElement("div");
+        const rowE = [];
+        for (let c = 0; c < num_cols; c++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.textContent = choose_random(char_set);
+            row.appendChild(cell);
+            rowE.push(cell);
+        }
+        div_matrix.appendChild(row);
+        grid.push(rowE);
+    }
+    return grid;
+};
+
+const main = async () => {
+    console.log(char_set, char_set.length);
+    const div_matrix = document.querySelector("#div-matrix");
+    console.log("div_matrix.style.width", div_matrix.style.width);
+    const div_matrix_rect = div_matrix.getBoundingClientRect();
+    console.log("div_matrix.getBoundingClientRect", div_matrix_rect);
+
+    const num_rows = 64;
+    const _num_cols = Math.floor(div_matrix_rect.width / 16); // 16px ~ 1em
+    const num_cols = Math.max(Math.min(_num_cols, 64), 1);
+    console.log("num_rows", num_rows, "num_cols", num_cols);
+    const max_waves = Math.floor((num_cols * num_cols) / 2); // 64 * 10;
+    const grid = reset(div_matrix, num_rows, num_cols);
+    let last_t = 0;
+    const t_speed = 50;
+    const waves = new Map();
+    let wave_id = 0;
+    const step = (t) => {
+        requestAnimationFrame(step);
+        if (t - last_t < t_speed) return;
+        last_t = t;
+        if (waves.size < max_waves && random_bool(0.5)) {
+            // waves.set(wave_id, { r: random_int(num_rows), c: random_int(num_cols) });
+            waves.set(wave_id, { r: 0, c: random_int(num_cols) });
+            wave_id++;
+        }
+        const to_be_removed = [];
+        for (let [k, wave] of waves) {
+            const { r, c } = wave;
+            // prev_cell.classList.remove("fade");
+            // cell.textContent = choose_random(char_set);
+            if (wave.r >= num_rows) {
+                to_be_removed.push(k);
+                continue;
+            }
+            const cell = grid[r][c];
+            cell.textContent = choose_random(char_set);
+            cell.classList.toggle("fade");
+            if (random_bool(0.05)) cell.classList.toggle("brighter");
+            wave.r++;
+        }
+        to_be_removed.forEach((k) => waves.delete(k));
+    };
+    requestAnimationFrame(step);
+};
+
+main().catch(console.error);
